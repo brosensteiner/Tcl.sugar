b4bb21b6eae6c83203a552386745f21b1d1825ec
Bernhard Rosensteiner <brosensteiner@gmail.com>
2012-05-11 15:41:24 +0200

    "execute script in terminal" fix", socket code completion

--- Scripts/exec-tcl-script-shell.sh
+++ Scripts/exec-tcl-script-shell.sh
@@ -4,13 +4,22 @@ exec osascript <<EOF
 
 tell application "Terminal"
 activate
-do script "tclsh" in window 1
-do script "set theFilePath $::env(EDITOR_DIRECTORY_PATH)/$::env(EDITOR_FILENAME)" in window 1
-do script "source \$theFilePath" in window 1
-do script "exit" in window 1
+
+do script "tclsh $E_FILEPATH" in window 1
+
 end tell
 
 EOF
 
-
+# exec osascript <<EOF
+# 
+# tell application "Terminal"
+# activate
+# do script "tclsh" in window 1
+# do script "set theFilePath $::env(EDITOR_DIRECTORY_PATH)/$::env(EDITOR_FILENAME)" in window 1
+# do script "source \$theFilePath" in window 1
+# do script "exit" in window 1
+# end tell
+# 
+# EOF
 
